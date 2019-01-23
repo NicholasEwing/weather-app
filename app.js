07e550c2a143f76e1ed3163f81656ffc8c9e7c88
Nicholas Ewing <nicholassewing@gmail.com>
2019-01-23 14:22:20 -0500

    Fix favicon for heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const favicon = require("serve-favicon");
 
 const indexRouter = require("./routes/index");
 
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico'));
 
 app.use("/", indexRouter);
 
