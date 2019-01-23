9972b5b39db9c69df78e7cbfcc9a9b83666bd70f
Nicholas Ewing <nicholassewing@gmail.com>
2019-01-23 15:02:26 -0500

    Fix app.js typo

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname,'public','images','favicon.ico'));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 app.use("/", indexRouter);
 
